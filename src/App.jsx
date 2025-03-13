3d70d6bdcbc34462a5769fd5ba2ecf8e5716f4c3
root404-ir <mohammadrg360@gmail.com>
2025-03-13 16:56:48 +0330

    design ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-
+import Weather from './component/Weather'
+import './style.css'
 function App() {
 
   return (
     <>
-
+      <Weather />
     </>
   )
 }
---
+++ src/component/Weather.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+const Weather = () => {
+    const [changeBg, setChangeBg] = useState('warm')
+    return (
+        <>
+            <div className={`app-container bg_${changeBg} flex flex-col items-center justify-center gap-20`}>
+                <div className="flex flex-col justify-center items-center gap-20 bg-amber-200 rounded-2xl p-10">
+                    <div>
+                        <div>
+                            <form>
+                                <input type="text" className="border-b font-bold text-blue-900 text-4xl py-2 px-4 rounded-xl" placeholder="نام شهر یا کشور" />
+                            </form>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="flex flex-col items-center gap-2">
+                            <h3>دوشنبه ۱۸ بهمن ۱۴۰۰</h3>
+                            <span>ساعت ۱۸:۰۰</span>
+                        </div>
+                    </div>
+                    <div className="bg-cyan-300 rounded-2xl py-5 px-10">
+                        <div>
+                            <span>۱۵</span>°C
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <h3>Cloudy</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Weather
