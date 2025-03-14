8409d24bc09ad8b6b487ba6eb95a12860ff0bf1e
root404-ir <mohammadrg360@gmail.com>
2025-03-15 00:02:14 +0330

    create weatherMaps

--- src/component/WeatherMaps.jsx
+++ src/component/WeatherMaps.jsx
@@ -1,7 +1,31 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 const WeatherMaps = () => {
+    const [weatherMap, setWeatherMap] = useState('')
+    useEffect(() => {
+        axios.get('https://one-api.ir/weather/?token=634793:67961f262e957&action=weathermaps&type=pressure', {
+            headers: {
+                "Content-Type": "image/png"
+            },
+            responseType: "blob"
+        }).then(res => {
+            const image = URL.createObjectURL(res.data)
+            setWeatherMap(image)
+        })
+        return () => {
+            URL.revokeObjectURL(weatherMap)
+        }
+    }, [weatherMap])
+
     return (
-        <div>WeatherMaps</div>
+        <>
+            {weatherMap ? (
+                <img src={weatherMap} alt="" />
+            ) : (
+                <h3>در حال دریافت تصویر...</h3>
+            )}
+        </>
     )
 }
 
