4852266f9f9d7b80df1d5853950a621eece4d4da
root404-ir <mohammadrg360@gmail.com>
2025-03-15 16:27:51 +0330

    add dist

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [
     react(),
     tailwindcss()
